e23e4d25f26788f9db3bfc4330c8b8feb8394053
Orn95 <artflash00@gmail.com>
2024-07-31 16:51:49 +0300

    Add files via upload

---
+++ kk.js
@@ -0,0 +1,47 @@
+let input = document.querySelector(".input");
+let power = "";
+
+function insert(num) {
+  if (input.textContent == 0) {
+    input.textContent = "";
+    input.textContent += num;
+  } else if (input.textContent == ".opertor") {
+    input.textContent = "";
+    input.textContent += num;
+  } else input.textContent += num;
+}
+
+function clean() {
+  input.textContent = "0";
+  power = "";
+}
+
+function equal() {
+  let exp = input.textContent;
+  if (input.textContent.includes("^")) {
+    let tmp = input.textContent.split("^");
+    let num = eval(power);
+    let pow = +tmp[1];
+    input.textContent = Math.pow(num, pow);
+    power = "";
+    return;
+  }
+  if (exp) {
+    input.textContent = eval(exp);
+  }
+}
+
+function operation(name) {
+  if (name == "sqrt") {
+    input.textContent = Math.sqrt(eval(input.textContent));
+  }
+  if (name == "^") {
+    power = input.textContent;
+    input.textContent += "^";
+  }
+}
+function Neg() {
+  if (input.textContent < 0) {
+    input.textContent = input.textContent * -1;
+  }
+}
